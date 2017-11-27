459d04102e890263552eaff8c1cf2239cf4dc1d4
bandofpv <andobernas@gmail.com>
2017-11-27 09:05:15 -0800

    Add files via upload

--- Arm.java
+++ Arm.java
@@ -2,9 +2,9 @@ import java.awt.*;
 import javax.swing.JFrame;
 
 public class Arm extends JFrame {
-	
+
 	double time, dt;
-	
+
 	public Arm() {
 		init();
 	}
@@ -16,35 +16,57 @@ public class Arm extends JFrame {
 		dt = 0.1;
 		repaint();
 	}
-	
+
 	public void paint(Graphics g) {
 		incrementTime();
 		drawCircle(g);
+		drawTriangle(g);
 		restTen();
 		eraseCircle(g);
+		eraseTriangle(g);
 		repaint();
 	}
-	
+
 	public void incrementTime(){
 		time += dt;
 	}
-	
+
 	public void drawCircle(Graphics g){
 		g.setColor(Color.BLACK);
 		g.drawOval(300, 50, (int)(50+time), (int)(50+time));
 	}
+
+	public void drawTriangle(Graphics g){
+		double xOfCircle = Math.cos(time);
+		double yOfCircle = Math.sin(time);
+		
+		int[] xp = {(int)(xOfCircle * 100.0) + 200, 100 + 200, 100 + 200};
+		int[] yp = {(int) (yOfCircle * 100.0) + 100, 50 + 100, 50 + 100};
+		g.drawPolygon(xp, yp, 3);
+		
+	}
 	
-	public void restTen(){
-		try {
-			Thread.sleep(10);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+	public void eraseTriangle(Graphics g){
+		double xOfCircle = Math.cos(time);
+		double yOfCircle = Math.sin(time);
+		
+		int[] xp = {(int)(xOfCircle * 100.0) + 200, 100 + 200, 100 + 200};
+		int[] yp = {(int) (yOfCircle * 100.0) + 100, 50 + 100, 50 + 100};
+		g.setColor(Color.WHITE);
+		g.drawPolygon(xp, yp, 3);
+	}
+
+		public void restTen(){
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void eraseCircle(Graphics g){
+			g.setColor(Color.WHITE);
+			g.drawOval(300, 50, (int)(50+time), (int)(50+time));
+
 		}
 	}
-	
-	public void eraseCircle(Graphics g){
-		g.setColor(Color.WHITE);
-		g.drawOval(300, 50, (int)(50+time), (int)(50+time));
-
-	}
-}
