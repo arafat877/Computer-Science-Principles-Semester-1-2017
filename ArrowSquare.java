3fe4389ce8946523f0cc5e3cc18a09525c8221ba
bandofpv <andobernas@gmail.com>
2017-12-08 09:44:33 -0800

    Add files via upload

--- ArrowSquare.java
+++ ArrowSquare.java
@@ -19,32 +19,49 @@ public class ArrowSquare extends JFrame {
 
 	public void paint(Graphics g) {
 		incrementTime();
-		arrowSquare(g);
+		left(g);
 		repaint();
 		System.out.println(time);
-		
+
 	}
-	
+
 	public void incrementTime(){
 		time += dt;
 	}
-	
+
 	public void arrowFirstSquare(Graphics g, int x, int y){
 		g.setColor(Color.BLACK);
 		g.drawRect((int) (100 - (x * time)) + 200, (int) (100 - (y * time)) + 100, 100, 200);
 	}
-	
+
+	public void eraseFirstSquare(Graphics g, int x, int y){
+		g.setColor(Color.WHITE);
+		g.drawRect((int) (100 - (x * time)) + 200, (int) (100 - (y * time)) + 100, 100, 200);
+	}
+
+	public void restTen(){
+		try {
+			Thread.sleep(10);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public void arrowSquare(Graphics g){
-		Scanner user_input = new Scanner(System.in);
-		
 		arrowFirstSquare(g,1,0);
-		
+		restTen();
+		eraseFirstSquare(g, 1,0);
+	}
+
+	public void left(Graphics g){
+		Scanner user_input = new Scanner(System.in);
 		String input;
 		System.out.print("Type:");
 		input = user_input.next();
-		
-		System.out.println("Your name is: " + input);
+
+		if (input == "w"){
+			arrowSquare(g);
+		}
 	}
 
 	public static void main(String[] args) {
